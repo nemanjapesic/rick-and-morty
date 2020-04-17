36689e86cb8ab6019d942a50aa0fb9cc80b7157b
Nemanja Pesic <nemanja992@gmail.com>
2020-04-17 20:18:37 +0200

    Updates

---
+++ precache-manifest.a5403bf7c26446f42eef3eee3148b622.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d4f467fb7ed1441d41c0844324799829",
+    "url": "/rick-and-morty/index.html"
+  },
+  {
+    "revision": "25bccadd8b06e372d61c",
+    "url": "/rick-and-morty/static/css/main.53c98b71.chunk.css"
+  },
+  {
+    "revision": "e7adbb7cb12626f284a6",
+    "url": "/rick-and-morty/static/js/2.f31d6d6a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/rick-and-morty/static/js/2.f31d6d6a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "25bccadd8b06e372d61c",
+    "url": "/rick-and-morty/static/js/main.f1b6e318.chunk.js"
+  },
+  {
+    "revision": "c1b7ac5fbc2c52e1e347",
+    "url": "/rick-and-morty/static/js/runtime-main.9ea3f8cd.js"
+  }
+]);
